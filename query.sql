762a840559af2faff652edc7ce4acdd4144bdf1d
tyedem <90783116+tyedem@users.noreply.github.com>
2022-02-11 10:25:04 -0500

    Analyze suspicious transactions between 7am and 9am

--- query.sql
+++ query.sql
@@ -35,7 +35,7 @@ WHERE amount < 2;
 SELECT * FROM less_than_2;
 --Result = 350 transactions
 -- Given some fraudsters hack a credit card by making several small $2.00 transactions, 
---there is strong likelihood there is some fraud occurring in this set of 350 transactions,
+--there is strong likelihood there is at least some fraud occurring in this set of 350 transactions,
 --which represents 10% of the total transactions in this dataset.
 
 --Create view of top 100 highest transactions  between 7:00am and 9:00am
@@ -45,4 +45,28 @@ FROM transaction as a
 WHERE date_part ('hour', date) >= 7 and date_part ('hour', date) < 9
 ORDER by amount DESC
 LIMIT 100;
--- There are 7 transactions over $1000, one at $748 and one at $100. The rest are less than $25.
+-- Between 7am and 9am there are 7 transactions over $1000, one at $748 and one at $100. The rest are less than $25.
+--High-cost purchases between 7am and 9am are suspicious as they are outlier purchases.
+---Smaller suspicious transactions are likely executed throughout the day to test-purchase before making a bigger fraudulent transaction.
+--I suspect there are greater number of fraudulent transactions after 9am in order to bury fraudulent transactions
+--amongst legitimate transactions in order to obfuscate detection.
+
+SELECT *
+FROM transaction as a
+WHERE date_part ('hour', date) >= 9
+ORDER by amount DESC;
+---When analyzing transactions made at or after 9am, most larger purchases are made after 12pm
+---with occasional larger purchases before 12pm.
+
+SELECT *
+FROM transaction as a
+WHERE date_part ('hour', date) >= 9
+ORDER by amount <=5 DESC;
+
+SELECT name, AS merchant
+FROM cardholder as a
+INNER JOIN credit_card as b ON a.id = b.cardholder_id
+INNER JOIN transaction as c ON b.card = c.card
+GROUP BY name
+ORDER BY name ASC;
+
